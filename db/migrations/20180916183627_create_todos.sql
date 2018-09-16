e74ee23051a99cd812dd4d8a42599f9167de5f2f
Alireza Bashiri <azbshiri@gmail.com>
2018-09-17 02:16:51 +0430

    Fix migration indentation

--- db/migrations/20180916183627_create_todos.sql
+++ db/migrations/20180916183627_create_todos.sql
@@ -1,8 +1,8 @@
 -- migrate:up
 create table if not exists todos (
-  id serial not null unique,
-  title varchar(255),
-  user_id integer,
+	id serial not null unique,
+	title varchar(255),
+	user_id integer,
 	created_at timestamp not null default now(),
 	updated_at timestamp
 );
